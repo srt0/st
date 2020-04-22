9b1be160fe8d4d6f8301d7b3e1cfc288dfc023e2
Saikat Tapu <saikat.tapu@gmail.com>
2020-04-22 18:32:42 +0600

    commit on 22 April, 2020

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.3;
+static float chscale = 1.2;
 
 /*
  * word delimiter string
