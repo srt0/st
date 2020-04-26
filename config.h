79d84bf9b45cdfb0153f8a883439a6d160b4ee7f
Saikat Tapu <saikat.tapu@gmail.com>
2020-04-26 18:44:44 +0600

    config.h once again

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Medium:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=13:antialias=true:autohint=true";
 static char *font2[] = {
+    "MesloLGS NF Regular:pixelsize=13:antialias=true:autohint=true",
     "JoyPixels:pixelsize=13:antialias=true:autohint=true"
 };
 static int borderpx = 2;
